3bf6acc26fb9e3210fa56bb5fa897085dd8be3ab
govindpnair <govidachu@gmail.com>
2025-06-10 16:53:53 +0530

    Pushed at 5pm on 10/6

---
+++ random password gen.js
@@ -0,0 +1,11 @@
+function generatePassword(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?';
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        password += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return password;
+}
+
+// Example usage:
+console.log("The password is "+generatePassword(12)); // Generates a 12-character random password
