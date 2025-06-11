9494e334076b1b8ac300d497a7246e98aeb0717f
govindpnair <govidachu@gmail.com>
2025-06-11 12:25:14 +0530

    Create PALINDROME.js

---
+++ PALINDROME.js
@@ -0,0 +1,13 @@
+let word = "Chicago";
+function palindrome(name){
+    let upperName = name.toUpperCase();
+    for (let i=0;i<(upperName.length)/2;i++)
+        if(upperName[i]==upperName[upperName.length-1])
+        return 1;
+        else
+        break;
+}
+if(palindrome(word)==1)
+    console.log("The word is palindrome");
+else
+    console.log("The word is not palindrome");
