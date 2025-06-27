8df31dd078bd2a84333eff937e9dbb2042e9033d
govindpnair <govidachu@gmail.com>
2025-06-27 16:46:43 +0530

    Update app.ts

--- Angular-pgms/calculator/src/app/app.ts
+++ Angular-pgms/calculator/src/app/app.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 })
 export class App {
   protected title = 'calculator';
-  result = "hi";
+  result = "";
   arriveResult(value:string)
   {
     return this.result+= value;
