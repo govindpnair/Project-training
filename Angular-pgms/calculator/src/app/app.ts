4cd24377b4fcfefdab8bebeb34054185fa8f6d15
govindpnair <govidachu@gmail.com>
2025-06-27 16:38:41 +0530

    Calculator-angular

---
+++ Angular-pgms/calculator/src/app/app.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  standalone: false,
+  styleUrl: './app.scss'
+})
+export class App {
+  protected title = 'calculator';
+  result = "hi";
+  arriveResult(value:string)
+  {
+    return this.result+= value;
+  }
+  calculator()
+  {
+    this.result = eval(this.result);
+  }
+
+  clear()
+  {
+    this.result = "";
+  }
+}
--- Angular-pgms/hotel-checkin-app/src/app/guest/guest.ts
+++ Angular-pgms/hotel-checkin-app/src/app/guest/guest.ts
@@ -17,15 +17,32 @@ export class Guest {
   guests: Traveller[] = [];
   newGuest: Traveller = {name: '', roomNumber:0}
 
+   ngOnInit(): void {
+    this.loadGuests();
+  }
+
    checkIn() {
     if (this.newGuest.name.trim() && this.newGuest.roomNumber > 0) {
       this.guests.push({ ...this.newGuest });
       this.newGuest = { name: '', roomNumber: 0 };
+      this.saveGuests();
     }
   }
 
   checkOut(index: number) {
     this.guests.splice(index, 1);
+    this.saveGuests();
+  }
+
+ private saveGuests(): void {
+    localStorage.setItem('guestList', JSON.stringify(this.guests));
+  }
+
+  private loadGuests(): void {
+    const saved = localStorage.getItem('guestList');
+    if (saved) {
+      this.guests = JSON.parse(saved);
+    }
   }
 
 }
